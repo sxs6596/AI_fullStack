2d39aa79d7ba42a2667dfdee3721d26fe006daf2
sxs6596 <sxs6596@mavs.uta.edu>
2025-10-30 19:31:22 -0500

    configured Open AI API KEY

---
+++ packages/server/index.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import type {Request, Response} from 'express'; 
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app = express(); 
+const port = process.env.PORT || 3000; 
+app.get('/',(req:Request, res:Response)=>{
+    res.send(`apiKey value is ${process.env.OPENAI_API_KEY}`);
+})
+
+app.listen(port, ()=>{
+    console.log(`server is running on the port ${port}`);
+})
